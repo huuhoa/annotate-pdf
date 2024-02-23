dd188a68eeeac0e216625fe0d4afd3df5b7fb70c
Huu Hoa NGUYEN <huuhoa@gmail.com>
2024-02-23 02:46:16 +0000

    chg: first working version

--- Hello.py
+++ Hello.py
@@ -15,36 +15,88 @@
 import streamlit as st
 from streamlit.logger import get_logger
 
+from pypdf import PdfWriter, PdfReader, Transformation
+import io
+from reportlab.pdfgen.canvas import Canvas
+
 LOGGER = get_logger(__name__)
+
+def create_annotated_pdf(text: str, pagesize, rotate_angle):
+#   page_to_merge = [0, 1, 4] #Refers to the First page of PDF 
+    xcoor = pagesize[0] - 40 #To be changed according to your pdf
+    ycoor = 40 #To be changed according to your pdf
+
+    ycoor = float(pagesize[1]) - ycoor
+    packet = io.BytesIO()
+    c = Canvas(packet,pagesize=pagesize)
+    #   c.setFillColorRGB(1,0,0) #choose your font colour
+    c.setFont("Helvetica", 15) #choose your font type and font size
+    c.drawRightString(xcoor, ycoor, text)
+    c.save()
+    packet.seek(0)
+    overlay_pdf = PdfReader(packet)
+    overlay = overlay_pdf.pages[0]
+    overlay.add_transformation(Transformation().
+                        rotate(rotate_angle))
+                        # .
+                        # translate(float(overlay.mediabox.width/2), 0))
+    return overlay_pdf
+
+
+def annotate_pdf_file(pdf_source, names: list):
+    input_pdf = PdfReader(pdf_source)
+    page_count = len(input_pdf.pages)
+    inputpdf_page_to_be_merged = input_pdf.pages[0]
+    pagesize=(inputpdf_page_to_be_merged.mediabox.width,inputpdf_page_to_be_merged.mediabox.height)
+    annotations = {n: create_annotated_pdf(n, pagesize, inputpdf_page_to_be_merged.get('/Rotate') or 0) for n in names}
+    pdf_sources = {n: PdfReader(pdf_source) for n in names}
+
+    output = PdfWriter()
+    for PAGE in range(page_count):
+        for n in names:
+            source_pdf = pdf_sources[n]
+            overlay = annotations[n]
+            source_page = source_pdf.pages[PAGE]
+            source_page.merge_page(overlay.pages[0])
+            output.add_page(source_page)
+
+    outputStream = io.BytesIO()
+    output.write(outputStream)
+    return outputStream.getvalue()
 
 
 def run():
     st.set_page_config(
-        page_title="Hello",
+        page_title="PDF Annotation",
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
+    st.write("# PDF Annotation")
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+        Thực hiện annotate và duplicate mỗi trang trong file PDF với các dòng text được input.
     """
     )
+
+    pdf_data = None
+    with st.form('upload_form'):
+        uploaded_pdf = st.file_uploader(label='Upload PDF file', accept_multiple_files=False, type='pdf')
+        names = st.text_area(label='Nhập tên cần dán lên file PDF. Mỗi dòng cho 1 tên', placeholder='Kitty\nMicky')
+        submitted =st.form_submit_button('Submit')
+        if submitted:
+            if uploaded_pdf is None:
+                st.write('Must select PDF file first')
+                return
+            if not names:
+                st.write('Must input names')
+                return
+            name_list = names.splitlines()
+            pdf_data = annotate_pdf_file(uploaded_pdf, name_list)
+
+    if pdf_data:
+        st.write('Bấm nút download phía dưới để download kết quả')
+        st.download_button('Download PDF', data=pdf_data, file_name='result.pdf', mime='application/pdf')
 
 
 if __name__ == "__main__":
